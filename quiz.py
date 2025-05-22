5a07e6da6e14ca1bd2aa7483579b9b1f4c15d260
Eunice <reyes05eunice@gmail.com>
2025-05-23 01:16:22 +0800

    added class Quiz

---
+++ quiz.py
@@ -0,0 +1,28 @@
+class Quiz:
+    def __init__(self, title, questions):
+        self.title = title
+        self.questions = questions
+        self.score = 0
+    
+    def start_quiz(self):
+        print(f"Starting quiz '{self.title}'....\n")
+        for idx, question in enumerate(self.questions, start=1):
+            print(f"\nQuestion {idx}: {question['question']}\n" \
+            f"a) {question['a']}\n" \
+            f"b) {question['b']}\n" \
+            f"c) {question['c']}\n" \
+            f"d) {question['d']}\n")
+            user_answer = input("Your answer (a/b/c/d): ").strip().lower()
+
+        if question.is_correct(user_answer):
+            print("\nYIPPEEEEEE CORRECT!!!! Thank you so much for helping me.")
+            self.score += 1 
+            print(f"\t\tScore: {self.score}")
+        else:
+            print("No.... noooooooo no no no nO NO NO NO NO NO NO")
+            print(f"\nThe correct answer is: {question.correct_answer['answer']}")
+            print(f"\t\tScore: {self.score}")
+
+        print(f"Quiz complete! Your total score: {self.score}/{len(self.questions)}")
+
+        
