76db8d3cb3f27e46679a6ae0f3b2938ed6bfe401
Eunice <reyes05eunice@gmail.com>
2025-05-23 01:03:07 +0800

    changed file name from main_menu.py to questions.py and added class Question

---
+++ questions.py
@@ -0,0 +1,9 @@
+class Question:
+    def __init__(self, text, options, correct_option):
+        self.text = text
+        self.options = options
+        self.correct_option = correct_option
+
+    def is_correct(self, answer):
+        return answer.lower().strip() == self.correct_option.lower().strip()
+    
