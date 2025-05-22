573a9b5f537eaf9653db3e46a2c628a2ceb66fa2
Eunice <reyes05eunice@gmail.com>
2025-05-23 01:33:10 +0800

    added class Instructions

---
+++ instructions.py
@@ -0,0 +1,26 @@
+import time
+
+class Instructions:
+    def show_instructions(self):
+        while True:
+            print("\nQuiz Simulator: Simple, Easy, Fresh!!\n" \
+            "\nTo create a quiz, select option 2 in the menu. Enter your desired quiz title and input your questions, answers, and the correct option. Press 0 to exit the quiz creator mode and your data will be stored as a .txt file in your files.\n" \
+            "\nTo take a quiz, select option 3 in the menu. Type in the full file name of your quiz and start answering!\n" \
+            "\nTo exit this program, select option 4 in the menu. Sad to see you go this early :((\n"
+            "\nWithout further the ado (ooohh,,, the ado furthens), let's get our thinking caps on and start quizzing!!\n\n")
+            time.sleep(10)
+            print("I can hear them.")
+            time.sleep(2)
+            print("I haven't had food and water for 2 days.")
+            time.sleep(2)
+            print("I'm losing some feeling in my limbs.")
+            time.sleep(2)
+            print("But I must keep coding.")
+            time.sleep(3)
+            print("For them.")
+            time.sleep(2)
+            print("....")
+            
+            for idx in range(0, 20):
+                time.sleep(0.3)
+                print("THEY'RE COMING")
