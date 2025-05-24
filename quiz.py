52a8e79a9274aabce628cb7d07f00532c181f6fb
Eunice <reyes05eunice@gmail.com>
2025-05-24 12:03:43 +0800

    fixed scoring errors (indent error)

--- quiz.py
+++ quiz.py
@@ -7,21 +7,19 @@ class Quiz:
     def start_quiz(self):
         print(f"Starting quiz '{self.title}'....\n")
         for idx, question in enumerate(self.questions, start=1):
-            print(f"\nQuestion {idx}: {question['question']}\n" \
-            f"a) {question['a']}\n" \
-            f"b) {question['b']}\n" \
-            f"c) {question['c']}\n" \
-            f"d) {question['d']}\n")
+            print(f"\nQuestion {idx}: {question.text}")
+            for key, value in question.options.items():
+                print(f"{key}) {value}")
             user_answer = input("Your answer (a/b/c/d): ").strip().lower()
 
-        if question.is_correct(user_answer):
-            print("\nYIPPEEEEEE CORRECT!!!! Thank you so much for helping me.")
-            self.score += 1 
-            print(f"\t\tScore: {self.score}")
-        else:
-            print("No.... noooooooo no no no nO NO NO NO NO NO NO")
-            print(f"\nThe correct answer is: {question.correct_answer['answer']}")
-            print(f"\t\tScore: {self.score}")
+            if question.is_correct(user_answer):
+                print("\nYIPPEEEEEE CORRECT!!!! Thank you so much for helping me.")
+                self.score += 1 
+                print(f"\t\tScore: {self.score}")
+            else:
+                print("No.... noooooooo no no no nO NO NO NO NO NO NO")
+                print(f"\nThe correct answer is: {question.correct_option}")
+                print(f"\t\tScore: {self.score}")
 
         print(f"Quiz complete! Your total score: {self.score}/{len(self.questions)}")
 
